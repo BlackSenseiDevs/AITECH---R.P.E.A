bb597e797b53f7e58c7c809da00c7caedc17d22d
Antonio-Cleyton.N.d.S <81056840+BlackSenseiDevs@users.noreply.github.com>
2023-04-01 23:51:36 -0300

    Initial commit

---
+++ menu.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace AITECH___R.P.E.A
+{
+    partial class menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cADASTROToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fUNCIONARIOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cLIENTESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pRODUTOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nOVOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pESQUISARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fORNECEDORESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nOVOToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pESQUISARToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.fUNCIONARIOSToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cAIXAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fUNCIONARIOSToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.nOVOToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOMISSÃOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pESQUISARToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.dEVEDORESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uSUARIOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iNTERFACEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aJUDAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sUPORTEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cADASTROToolStripMenuItem,
+            this.pRODUTOSToolStripMenuItem,
+            this.fORNECEDORESToolStripMenuItem,
+            this.fUNCIONARIOSToolStripMenuItem1,
+            this.aJUDAToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cADASTROToolStripMenuItem
+            // 
+            this.cADASTROToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fUNCIONARIOSToolStripMenuItem,
+            this.cLIENTESToolStripMenuItem});
+            this.cADASTROToolStripMenuItem.Name = "cADASTROToolStripMenuItem";
+            this.cADASTROToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.cADASTROToolStripMenuItem.Text = "CLIENTE";
+            // 
+            // fUNCIONARIOSToolStripMenuItem
+            // 
+            this.fUNCIONARIOSToolStripMenuItem.Name = "fUNCIONARIOSToolStripMenuItem";
+            this.fUNCIONARIOSToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fUNCIONARIOSToolStripMenuItem.Text = "NOVO";
+            this.fUNCIONARIOSToolStripMenuItem.Click += new System.EventHandler(this.fUNCIONARIOSToolStripMenuItem_Click);
+            // 
+            // cLIENTESToolStripMenuItem
+            // 
+            this.cLIENTESToolStripMenuItem.Name = "cLIENTESToolStripMenuItem";
+            this.cLIENTESToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cLIENTESToolStripMenuItem.Text = "PESQUISAR";
+            // 
+            // pRODUTOSToolStripMenuItem
+            // 
+            this.pRODUTOSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nOVOToolStripMenuItem,
+            this.pESQUISARToolStripMenuItem});
+            this.pRODUTOSToolStripMenuItem.Name = "pRODUTOSToolStripMenuItem";
+            this.pRODUTOSToolStripMenuItem.Size = new System.Drawing.Size(78, 20);
+            this.pRODUTOSToolStripMenuItem.Text = "PRODUTOS";
+            // 
+            // nOVOToolStripMenuItem
+            // 
+            this.nOVOToolStripMenuItem.Name = "nOVOToolStripMenuItem";
+            this.nOVOToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nOVOToolStripMenuItem.Text = "NOVO";
+            // 
+            // pESQUISARToolStripMenuItem
+            // 
+            this.pESQUISARToolStripMenuItem.Name = "pESQUISARToolStripMenuItem";
+            this.pESQUISARToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pESQUISARToolStripMenuItem.Text = "PESQUISAR";
+            // 
+            // fORNECEDORESToolStripMenuItem
+            // 
+            this.fORNECEDORESToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nOVOToolStripMenuItem1,
+            this.pESQUISARToolStripMenuItem1});
+            this.fORNECEDORESToolStripMenuItem.Name = "fORNECEDORESToolStripMenuItem";
+            this.fORNECEDORESToolStripMenuItem.Size = new System.Drawing.Size(106, 20);
+            this.fORNECEDORESToolStripMenuItem.Text = "FORNECEDORES";
+            // 
+            // nOVOToolStripMenuItem1
+            // 
+            this.nOVOToolStripMenuItem1.Name = "nOVOToolStripMenuItem1";
+            this.nOVOToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.nOVOToolStripMenuItem1.Text = "NOVO";
+            // 
+            // pESQUISARToolStripMenuItem1
+            // 
+            this.pESQUISARToolStripMenuItem1.Name = "pESQUISARToolStripMenuItem1";
+            this.pESQUISARToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.pESQUISARToolStripMenuItem1.Text = "PESQUISAR";
+            // 
+            // fUNCIONARIOSToolStripMenuItem1
+            // 
+            this.fUNCIONARIOSToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cAIXAToolStripMenuItem,
+            this.fUNCIONARIOSToolStripMenuItem2,
+            this.dEVEDORESToolStripMenuItem,
+            this.uSUARIOSToolStripMenuItem,
+            this.iNTERFACEToolStripMenuItem});
+            this.fUNCIONARIOSToolStripMenuItem1.Name = "fUNCIONARIOSToolStripMenuItem1";
+            this.fUNCIONARIOSToolStripMenuItem1.Size = new System.Drawing.Size(61, 20);
+            this.fUNCIONARIOSToolStripMenuItem1.Text = "GESTÃO";
+            // 
+            // cAIXAToolStripMenuItem
+            // 
+            this.cAIXAToolStripMenuItem.Name = "cAIXAToolStripMenuItem";
+            this.cAIXAToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cAIXAToolStripMenuItem.Text = "CAIXA";
+            // 
+            // fUNCIONARIOSToolStripMenuItem2
+            // 
+            this.fUNCIONARIOSToolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nOVOToolStripMenuItem2,
+            this.cOMISSÃOToolStripMenuItem,
+            this.pESQUISARToolStripMenuItem2});
+            this.fUNCIONARIOSToolStripMenuItem2.Name = "fUNCIONARIOSToolStripMenuItem2";
+            this.fUNCIONARIOSToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.fUNCIONARIOSToolStripMenuItem2.Text = "FUNCIONARIOS";
+            // 
+            // nOVOToolStripMenuItem2
+            // 
+            this.nOVOToolStripMenuItem2.Name = "nOVOToolStripMenuItem2";
+            this.nOVOToolStripMenuItem2.Size = new System.Drawing.Size(134, 22);
+            this.nOVOToolStripMenuItem2.Text = "NOVO";
+            // 
+            // cOMISSÃOToolStripMenuItem
+            // 
+            this.cOMISSÃOToolStripMenuItem.Name = "cOMISSÃOToolStripMenuItem";
+            this.cOMISSÃOToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.cOMISSÃOToolStripMenuItem.Text = "COMISSÃO";
+            // 
+            // pESQUISARToolStripMenuItem2
+            // 
+            this.pESQUISARToolStripMenuItem2.Name = "pESQUISARToolStripMenuItem2";
+            this.pESQUISARToolStripMenuItem2.Size = new System.Drawing.Size(134, 22);
+            this.pESQUISARToolStripMenuItem2.Text = "PESQUISAR";
+            // 
+            // dEVEDORESToolStripMenuItem
+            // 
+            this.dEVEDORESToolStripMenuItem.Name = "dEVEDORESToolStripMenuItem";
+            this.dEVEDORESToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dEVEDORESToolStripMenuItem.Text = "DEVEDORES";
+            // 
+            // uSUARIOSToolStripMenuItem
+            // 
+            this.uSUARIOSToolStripMenuItem.Name = "uSUARIOSToolStripMenuItem";
+            this.uSUARIOSToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.uSUARIOSToolStripMenuItem.Text = "USUÁRIOS";
+            // 
+            // iNTERFACEToolStripMenuItem
+            // 
+            this.iNTERFACEToolStripMenuItem.Name = "iNTERFACEToolStripMenuItem";
+            this.iNTERFACEToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.iNTERFACEToolStripMenuItem.Text = "INTERFACE";
+            // 
+            // aJUDAToolStripMenuItem
+            // 
+            this.aJUDAToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sUPORTEToolStripMenuItem});
+            this.aJUDAToolStripMenuItem.Name = "aJUDAToolStripMenuItem";
+            this.aJUDAToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.aJUDAToolStripMenuItem.Text = "AJUDA";
+            // 
+            // sUPORTEToolStripMenuItem
+            // 
+            this.sUPORTEToolStripMenuItem.Name = "sUPORTEToolStripMenuItem";
+            this.sUPORTEToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sUPORTEToolStripMenuItem.Text = "SUPORTE";
+            // 
+            // menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "menu";
+            this.Text = "R.P.E.A";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cADASTROToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fUNCIONARIOSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cLIENTESToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pRODUTOSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nOVOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pESQUISARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fORNECEDORESToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nOVOToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem pESQUISARToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem fUNCIONARIOSToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem cAIXAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fUNCIONARIOSToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem nOVOToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem cOMISSÃOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pESQUISARToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem dEVEDORESToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uSUARIOSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iNTERFACEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aJUDAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sUPORTEToolStripMenuItem;
+    }
+}
---
+++ menu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AITECH___R.P.E.A
+{
+    public partial class menu : Form
+    {
+        public menu()
+        {
+            InitializeComponent();
+        }
+
+        private void fUNCIONARIOSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
